abdc562eb6effb7d79908eea9a57392bfc211d3d
saurabhsingh <aman.define@gmail.com>
2021-07-23 14:51:31 +0530

    kafka streaming

--- kafka-publish.py
+++ kafka-publish.py
@@ -6,28 +6,9 @@ from kafka import KafkaProducer
 from json import dumps
 import config as conf
 
-auth = tweepy.OAuthHandler(conf.api_key, conf.api_secret)
-auth.set_access_token(conf.access_token, conf.access_token_secret)
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: dumps(x).encode('utf-8')
                          )
-api = tweepy.API(auth)
-
-
-def search_tweet():
-    search_result = api.search(q='Corona', lang='en', rpp=1)
-
-    for tweet in search_result:
-        data = tweet._json
-        tw = {
-            'id': data['id'],
-            'tweet': data['text'],
-            'Creation_date': data['created_at'],
-            'UserName': data['user']['name']
-        }
-        producer.send('tweet', tw)
-    producer.flush()
-
 
 def fake_date():
     fake = Faker()
@@ -46,6 +27,5 @@ def fake_date():
 if __name__ == '__main__':
     while True:
         print("Publish new tweets: ")
-        search_tweet()
-        # fake_date()
+        fake_date()
         time.sleep(5)
